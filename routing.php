97deed3633d7bbc0269d07d3f346c27402288ac7
bpkopania <83422290+bpkopania@users.noreply.github.com>
2022-12-20 09:17:36 +0100

    Initial commit

---
+++ routing.php
@@ -0,0 +1,14 @@
+<?php
+
+$routing = [
+    '/' => 'products',
+    '/products' => 'products',
+    '/view' => 'product',
+    '/edit' => 'edit',
+    '/delete' => 'delete',
+    '/cart' => 'cart',
+    '/cart/clear' => 'clear_cart',
+    '/cart/add' => 'add_to_cart',
+    
+    '/galery' => 'galery',
+];
---
+++ views/galery.php
@@ -0,0 +1,59 @@
+<!Doctype html>
+<html>
+    <head>
+        <title>wiatr w żagle</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="Stylesheet" href="static/css/style.css">
+        <link rel="Stylesheet" href="static/css/galery.css">
+
+    </head>
+    <body>
+        <header>
+            <h1>
+                <img src="svg/logo.svg" class="logo" alt="logotyp svg">
+                WIATR W ŻAGLE
+            </h1>
+        </header>
+
+        <nav>
+            <div><a href="index.html">Strona główna</a></div>
+            <div class="dropdown">
+                <a class="dropbtn active">Galeria</a>
+                <div class="dropdown-content">
+                    <a href="videos.html">Multimedia</a>
+                </div>
+            </div>
+            <div><a href="licenses.html">Uprawnienia</a></div>
+            <div><a href="test.html">Przetestuj swoją wiedzę</a></div>
+            <div><a href="index.html#bottom">Kontakt</a></div>
+
+        </nav>
+
+        <main>
+            <section class="animated-grid">
+                <div class="card">aaa</div>
+                <div class="card">aaa</div>
+                <div class="card">aaa</div>
+                <div class="card">aaa</div>
+                <div class="card">aaa</div>
+                <div class="card">aaa</div>
+                <div class="card">aaa</div>
+                <div class="card">aaa</div>
+                <div class="card">aaa</div>
+                <div class="card">aaa</div>
+                <div class="card">aaa</div>
+                <div class="card">aaa</div>
+                <div class="card">aaa</div>
+              </section>
+        </main>
+        
+        <footer>
+            <span id="bottom"></span>
+            2022, Bartosz Kopania, s193169<br>
+            Strona zrealizowana na potrzeby projektu z hih i wai<br>
+            Telefon: <a href="tel:123456789">+48 123-456-789</a><br>
+            Adres e-mail: <a href="mailto:s193169@student.pg.edu.pl">s193169@student.pg.edu.pl</a>
+        </footer>
+    </body>
+</html>
