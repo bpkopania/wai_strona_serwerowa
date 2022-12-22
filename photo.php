da92eb63ccc3da8baea4c9d60102c0c51743598b
bpkopania <83422290+bpkopania@users.noreply.github.com>
2022-12-22 20:25:55 +0100

    miniPhoto

--- photo.php
+++ photo.php
@@ -6,24 +6,70 @@
         $dir = '/var/www/dev/src/web/images/';
 
         $file = $_FILES['photo'];
-        $file_name = basename($file['name']);
-
-        $destiny = $dir . $file_name;
-
-        $tmp_path = $file['tmp_name'];//$file name form checker
-
-        $file_info = finfo_open(FILEINFO_MIME_TYPE);
-        $mime_type = finfo_file($file_info,$tmp_path);
-
-        if($mime_type === 'image/jpeg')
+        
+        if($file != NULL)
         {
-            echo "poprawny format";
+            $file_name = basename($file['name']);
+
+            $destiny = $dir . $file_name;
+
+            $tmp_path = $file['tmp_name'];//$file name from checker
+
+            $file_info = finfo_open(FILEINFO_MIME_TYPE);
+            $mime_type = finfo_file($file_info,$tmp_path);
+
+            if($mime_type === 'image/jpeg' || $mime_type === 'image/png')
+            {
+                //echo "poprawny format";
+                if(move_uploaded_file($tmp_path, $destiny))
+                {
+                    //echo "Upload przebiegł pomyślnie!\n";
+                }
+            }
         }
+        
+        //resize_image($file, $dir, $file_name);
+    }
 
+    function resize_image_png($photo, $dir, $name)
+    {
+        $src = $dir . $name;
+        $photo = imagecreatefrompng($src);
 
-        if(move_uploaded_file($tmp_path, $destiny))
+        $miniPhoto = imagescale(
+            $photo,
+            200,
+            125
+        );
+
+        $destiny = $dir . "mini_" . $name;
+
+        imagepng($miniPhoto, $destiny);
+    }
+
+    function resize_image_jpg($photo, $dir, $name)
+    {
+        $src = $dir . $name;
+        $photo = imagecreatefromjpeg($src);
+
+        $miniPhoto = imagescale(
+            $photo,
+            200,
+            125
+        );
+
+        $destiny = $dir . "mini_" . $name;
+
+        imagejpeg($miniPhoto, $destiny);
+    }
+
+    function add_waterMark($photo, $dir, $name)
+    {
+        $waterMark = $_POST["waterMark"];
+        $textcolor = imagecolorallocate($photo, 255, 255, 255);
+        if($waterMark != "")
         {
-            echo "Upload przebiegł pomyślnie!\n";
+            imagestring($photo, 5, 0, 0, 'Hello world!', $textcolor);
         }
     }
 
--- views/sender_view.php
+++ views/sender_view.php
@@ -5,16 +5,52 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="Stylesheet" href="static/css/style.css">
-        <link rel="Stylesheet" href="static/css/galery.css">
+        <link rel="Stylesheet" href="static/css/sender.css">
 
     </head>
     <body>
         <?php include_once '../views/static/header.php'?>
 
-        <form method="post" enctype="multipart/form-data" action="send">
-            <input type="file" name="photo"/>
-            <input type="submit" value="Prześlij"/>
-        </form>
+        <main>
+            <div class="form">
+                <form method="post" enctype="multipart/form-data" action="send">
+                    <input type="file" name="photo"/><br>
+                    <input type="text" name="waterMark"/><br>
+                    <input type="hidden" name="login"/><br>
+                    <input type="submit" value="Prześlij"/><br>
+                </form>
+            </div>
+
+            <div class="login">
+            <p>Zaloguj się</p>
+                <form action="login.php" method="post">
+                    <label for="login">Login:</label>
+                    <input type="text" id="loginLogin" name="loginLogin">
+                    <br>
+                    <label for="psw">Hasło:</label>
+                    <input type="password" id="pswLogin" name="pswLogin">
+                    <br>
+                    <input type="submit" value="Zaloguj się">
+                </form>
+                <br>
+                <p>Zarejestruj się</p>
+                <form action="register.php" method="post">
+                    <label for="login">Login:</label>
+                    <input type="text" id="login" name="login">
+                    <br>
+                    <label for="psw">Hasło:</label>
+                    <input type="password" id="psw" name="psw">
+                    <br>
+                    <label for="psw_r">Powtórz Hasło:</label>
+                    <input type="password" id="psw_r" name="psw_r">
+                    <br>
+                    <input type="submit" value="Zaloguj się">
+                    <input type="reset" value="Od nowa">
+                </form>
+            </div>
+        </main>
+        
+        
         
         <?php include_once '../views/static/footer.php'?>
     </body>
